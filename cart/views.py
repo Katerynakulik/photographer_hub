d6e9fae9db9bf51c8b1fc6f89f466af291ef74b9
Kateryna Kulik <katerynakulik4@gmail.com>
2026-04-01 13:00:40 +0200

    Add login page, start to make basket logic

--- cart/views.py
+++ cart/views.py
@@ -1,24 +1,34 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from products.models import PhotoProduct
 
 def view_cart(request):
-    cart = request.session.get("cart", {})
-    products = PhotoProduct.objects.filter(id__in=cart.keys())
-    total = sum(p.price for p in products)
-    return render(request, "cart/cart.html", {"products": products, "total": total})
+    """ Сторінка перегляду вмісту кошика """
+    return render(request, 'cart/cart.html')
 
-def add_to_cart(request, product_id):
-    """ Додає товар у сесію кошика """
-    product = get_object_or_404(PhotoProduct, id=product_id)
+def add_to_cart(request, item_id):
+    """ Додавання товару в кошик """
+    product = get_object_or_404(PhotoProduct, pk=item_id)
     cart = request.session.get('cart', {})
 
-    # Додаємо товар (або збільшуємо кількість, хоча для фото зазвичай 1)
-    if product_id in list(cart.keys()):
-        messages.info(request, f"{product.title} вже у вашому кошику.")
+    if item_id in list(cart.keys()):
+        messages.info(request, f"{product.title} is already in your cart.")
     else:
-        cart[product_id] = 1
-        messages.success(request, f"Додано {product.title} у кошик!")
+        cart[item_id] = 1
+        messages.success(request, f"Added {product.title} to your cart!")
 
     request.session['cart'] = cart
-    return redirect(request.META.get('HTTP_REFERER', 'home'))
+    return redirect(request.POST.get('redirect_url', reverse('home')))
+
+def remove_from_cart(request, item_id):
+    """ Видалення товару """
+    try:
+        cart = request.session.get('cart', {})
+        product = get_object_or_404(PhotoProduct, pk=item_id)
+        cart.pop(item_id)
+        messages.success(request, f"Removed {product.title} from your cart")
+        request.session['cart'] = cart
+        return redirect(reverse('view_cart'))
+    except Exception as e:
+        messages.error(request, f"Error removing item: {e}")
+        return redirect(reverse('view_cart'))
